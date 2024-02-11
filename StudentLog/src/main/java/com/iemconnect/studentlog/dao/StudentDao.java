f8fa1dca451c524614afcba38ca4195d4a44936e
aniket kumar <94619760+aniketkr2002@users.noreply.github.com>
2024-02-11 21:58:55 +0530

    Delete StudentLog directory

--- StudentLog/src/main/java/com/iemconnect/studentlog/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.iemconnect.studentlog.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.iemconnect.studentlog.model.Student;
-
-public interface StudentDao extends CrudRepository<Student,Integer> {
-	
-}
